c425abf5973250bd209ac7887a0acdeab14d2089
basil daboor <basildaboor12@gmail.com>
2023-10-04 12:39:44 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards(array, eventName) {
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+      newArr[i] = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return newArr;
+  }
+
+  
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
